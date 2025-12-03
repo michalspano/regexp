e553d4cf1fbb1e49b8e9a2305c99fb190b2d8426
Philipp-Immanuel Holst <philipp.i.holst@gmail.com>
2025-12-03 15:30:41 +0100

    added parantheses check for our regex
    Co-authored-by: Michal Spano michal.spano03@gmail.com

--- Main.hs
+++ Main.hs
@@ -2,26 +2,33 @@ module Main where
 
 import Parsing
 
-import Test.QuickCheck
+-- import Test.QuickCheck
 import Debug.Trace (trace)
 
 import Prelude hiding (concat)
 import Data.Char (isAlphaNum)
 
-newtype RegexStr = RegexStr String
-
-instance Arbitrary RegexStr where
-    arbitrary = undefined
 
 data Regex
     = Epsilon
     | Literal Char
     | Kleene Regex
     | Concat Regex Regex
+    -- | Optional Regex
+    -- | Plus Regex
     deriving (Eq, Show)
 
--- Kleene (Concat r1 r2)
--- (ab)* (ab, ab, ab, ab,...)
+pp :: Regex -> String 
+pp Epsilon = "()"
+pp (Literal l) = [l]
+pp (Kleene l@(Literal _)) = pp l ++ "*"
+pp (Kleene r) = "(" ++ pp r ++ ")*"
+pp (Concat r1 r2) = pp r1 ++ pp r2
+
+-- instance Arbitrary Regex where
+--    arbitrary = undefined
+
+-- TODO: allow extra brackets around expression even if unnecessary
 
 {-
 - eps
@@ -34,16 +41,39 @@ data Regex
 parseReg :: String -> Maybe(Regex,String)
 parseReg = parse reg
 
+
+
 reg :: Parser Regex
-reg  =  concat
+reg = reg1 <|> reg2
+
+reg1 :: Parser Regex
+reg1  =  concat
     <|> kleene
+    <|> plus
+    <|> optional 
     <|> literal
     <|> eps
 
+reg2 :: Parser Regex
+reg2  = do 
+    char '('
+    r <- reg1 <|> reg2
+    char ')'
+    return r
+
 regWithOutConcat :: Parser Regex
 regWithOutConcat = kleene
+                <|> plus
+                <|> optional 
                 <|> literal
                 <|> eps
+
+regWithoutEpsilon :: Parser Regex
+regWithoutEpsilon  =  concat
+    <|> kleene
+    <|> plus
+    <|> optional 
+    <|> literal
 
 eps :: Parser Regex
 eps = do
@@ -62,7 +92,7 @@ kleene = kleene1 <|> kleene2
 kleene1 :: Parser Regex
 kleene1 = do
     char '('
-    r <- reg
+    r <- reg 
     char ')'
     char '*'
     return $ Kleene r
@@ -79,12 +109,35 @@ concat = do
     tail_rs <- zeroOrMore reg
     return $ foldl Concat head_r tail_rs
 
+optional :: Parser Regex
+optional = do 
+    literal <|> eps 
+
+plus :: Parser Regex 
+plus = plus1 <|> plus2
+
+plus1 :: Parser Regex
+plus1 = do
+    char '('
+    r <- regWithoutEpsilon
+    char ')'
+    char '+'
+    return $ Concat r (Kleene r) 
+
+plus2 :: Parser Regex
+plus2 = do
+    r <- literal
+    char '+'
+    return $ Concat r (Kleene r)
+
 main :: IO ()
 main = do
     putStr ">>> "
     p <- getLine
     case parseReg p of
         Nothing      -> putStrLn "Failed to parse."
-        Just (p',"") -> print p'
-        Just (p', rest) -> putStrLn $ "Error: Input not read fully. Rest: " ++ show rest
+        Just (p',"") -> do { print p' ; putStrLn $ pp p' }
+        Just (p', rest) -> do
+            putStrLn $ "Error: Input not read fully. Rest: " ++ show rest 
+            putStrLn $ pp p'
     main
